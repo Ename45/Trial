bb09e58b62c3b2229804478bf3a4f1ba674069e3
Ename45 <enamesit@gmail.com>
2022-07-23 07:37:34 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,71 @@
+const colorapple = document.getElementById("color-apple");
+
+colorapple.addEventListener("click", (event) =>  {
+  event.target.style.background = "linear-gradient(135deg,  #67d40eb9, #db2c0dc7)"; 
+  event.target.style.width = "92%"; 
+  event.target.style.margin = "auto";
+  event.target.style.color = "black";
+})
+
+const coloravocado = document.getElementById("color-avocado");
+
+coloravocado.addEventListener("click", (event) =>  {
+  event.target.style.background = "green"; 
+  event.target.style.width = "92%"; 
+  event.target.style.margin = "auto";
+  event.target.style.color = "white";
+})
+
+const colorberry = document.getElementById("color-berry");
+
+colorberry.addEventListener("click", (event) =>  {
+  event.target.style.background = "linear-gradient(135deg,  #0d09f5, #db0d0d, #000000d3)"; 
+  event.target.style.width = "92%"; 
+  event.target.style.margin = "auto";
+  event.target.style.color = "black";
+})
+
+const colorcoconut = document.getElementById("color-coconut");
+
+colorcoconut.addEventListener("click", (event) =>  {
+  event.target.style.background = "linear-gradient(120deg,  #fdfdfd, #471a1a)"; 
+  event.target.style.width = "92%"; 
+  event.target.style.margin = "auto";
+  event.target.style.color = "white";
+})
+
+const colorlemon = document.getElementById("color-lemon");
+
+colorlemon.addEventListener("click", (event) =>  {
+  event.target.style.background = "#f5d103ef"; 
+  event.target.style.width = "92%"; 
+  event.target.style.margin = "auto";
+  event.target.style.color = "black";
+})
+
+const colorpeach = document.getElementById("color-peach");
+
+colorpeach.addEventListener("click", (event) =>  {
+  event.target.style.background = "linear-gradient(125deg,  #ec7c12, #db2c0dc7)"; 
+  event.target.style.width = "92%"; 
+  event.target.style.margin = "auto";
+  event.target.style.color = "white";
+})
+
+const colorpineapple = document.getElementById("color-pineapple");
+
+colorpineapple.addEventListener("click", (event) =>  {
+  event.target.style.background = "linear-gradient(135deg,  #f4a460, #a0512d86, #019101e0)"; 
+  event.target.style.width = "92%"; 
+  event.target.style.margin = "auto";
+  event.target.style.color = "black";
+})
+
+const colorwatermelon = document.getElementById("color-watermelon");
+
+colorwatermelon.addEventListener("click", (event) =>  {
+  event.target.style.background = "linear-gradient(135deg,  #47d111e0, #bcf357e0, #0e810ee0)"; 
+  event.target.style.width = "92%"; 
+  event.target.style.margin = "auto";
+  event.target.style.color = "white";
+})
